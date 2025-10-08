bd0c9e6120f577aa751db7b20ca32e9e9839cb6a
AhmedAljamal <gad59125@gmail.com>
2025-10-08 15:52:27 +0300

    Finishing Check Obsity App

---
+++ android/app/src/main/kotlin/com/example/check_obsity/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.check_obsity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Splash_Onboarding/splash.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:check_obsity/Screens/homepage.dart';
+import 'package:check_obsity/Splash_Onboarding/onboarding.dart';
+import 'package:check_obsity/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Call your timer function to navigate after 3 seconds
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  void startTimer() {
+    Timer(const Duration(seconds: 3), () {
+      // Navigate to the main screen after 3 seconds
+      if (onboardingShown) {
+        Get.offAll(() => const Homepage());
+      } else {
+        Get.offAll(() => const OnBoardingScreen());
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Image.asset(
+          "assets/images/applogo.png",
+          height: 300.h,
+        ),
+      ),
+    );
+  }
+}
